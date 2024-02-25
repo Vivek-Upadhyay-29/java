71d4c3f65207dec9a720935f527f7e91d686e34b
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-02-25 18:34:54 +0530

    var added

--- address.java
+++ address.java
@@ -1,13 +1,12 @@
 import java.net.*;
-public class address {
+public class address{
     public static void main(String[] args) {
         try{
-            InetAddress ip = InetAddress.getByName("www.google.com");
+            var ip = InetAddress.getByName("www.google.com");
             System.out.println("IP" + ip);
         }
         catch(UnknownHostException e){
             System.out.println(e);
         }
     }
-    
 }
