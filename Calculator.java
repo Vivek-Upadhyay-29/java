938e1ec07866751f0c4f3e0de6c420d2670def18
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-08-25 16:47:21 +0530

    calculator done

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out
+                .print(" Enter the value of a & b and select the operation: \n 1 for add \n 2 for sub \n 3 for multi");
+
+        int operation = sc.nextInt();
+        switch (operation) {
+            case 1:
+                System.out.println(a + b);
+                break;
+            case 2:
+                System.out.println(a - b);
+                break;
+            case 3:
+                System.out.println(a * b);
+                break;
+            default:
+                System.out.println("Enter a valid number");
+                break;
+        }
+    }
+}
