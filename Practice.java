da7eb3e58073e754c722f43949c0169d53943007
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-08-26 09:25:20 +0530

    commit

--- Practice.java
+++ Practice.java
@@ -1,10 +1,20 @@
 class Practice {
     public static void main(String[] args) {
         // syos
+        // int i = 10;
+        // for (i = 0; i <= 100; i++) {
+        // System.out.println("Welcome" + i);
+        // }
+        int i = 0;
+        // while (i < 111111) {
+        // System.out.println("welcome" + i);
+        // i++;
+        // }
 
-        for (int i = 0; i <= 100; i++) {
-            System.out.println("Welcome" + i);
-        }
+        do {
+            System.out.println("number" + i);
+            i++;
+        } while (i < 11);
 
     }
 }
