66558981de67da62bfd9a68dd67fb248f5c06ad1
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-08-27 17:21:07 +0530

    commit

---
+++ Pattern/Palondrom.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Palondrom {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = i; k >= 1; k--) {
+                System.out.print("*");
+            }
+
+            for (int m = 2; m <= i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
