7e26abf1517f0b7fada995d42afe0a9f49140647
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-08-31 13:25:34 +0530

    commit

---
+++ Array/Multiarray.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class Multiarray {
+    public static void main(String args[]) {
+        System.out.println("Enter rows and columns");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int colums = sc.nextInt();
+        int numbers[][] = new int[rows][colums];
+
+        // input
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                System.out.println(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
