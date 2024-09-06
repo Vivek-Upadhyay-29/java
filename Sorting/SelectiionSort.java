9f1d4ef7feaa7d3750a453affe8ca04a6ec3c10a
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-09-06 07:35:05 +0530

    commit

--- Sorting/SelectiionSort.java
+++ Sorting/SelectiionSort.java
@@ -1,6 +1,14 @@
 package Sorting;
 
 public class SelectiionSort {
+
+    public static void printArry(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 4, 8, 9, 2, 1 };
@@ -9,12 +17,15 @@ public class SelectiionSort {
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[i] > arr[i + 1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
+        printArry(arr);
+
     }
+
 }
---
+++ Sorting/selectionsort.java
@@ -0,0 +1,7 @@
+package Sorting;
+
+public class selectionsort {
+    public static void main(String[] args) {
+
+    }
+}
