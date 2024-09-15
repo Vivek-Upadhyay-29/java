3bd4e45da296f54545ca1fc27ceb873a18d1f028
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-09-15 19:00:18 +0530

    commit

--- String/stri.java
+++ String/stri.java
@@ -2,7 +2,6 @@ public class stri {
     public static void main(String[] args) {
 
         // String name = "Vivek";
-
         // int len = name.length() - 1;
         // String newNmae = "";
         // for (int i = len; i >= 0; i--) {
@@ -10,13 +9,27 @@ public class stri {
         // }
         // System.out.println(newNmae);
 
+        // String name = "Vivek";
+
+        // StringBuilder info = new StringBuilder();
+        // for (int i = name.length() - 1; i >= 0; i--) {
+        // info.append(name.charAt(i) + " ");
+        // }
+        // System.out.println(info.toString());
+
         String name = "Vivek";
+        String reverse = "";
 
-        StringBuilder info = new StringBuilder();
         for (int i = name.length() - 1; i >= 0; i--) {
-            info.append(name.charAt(i) + " ");
-        }
-        System.out.println(info.toString());
 
+            reverse += name.charAt(i);
+
+        }
+        System.out.println(reverse);
+
+        // for (int i = name.length() - 1; i >= 0; i--) {
+        // reverse += reverse.charAt(i);
+        // }
+        // System.out.println(reverse);
     }
 }
